eb0e30a729ae4b296ba51c90701cf80664cac4d9
harshith <gharshith578@gmail.com>
2025-09-18 14:31:24 +0530

    2nd commit

---
+++ src/ScannerClass_BufferDemo/ScannerClassDemo.java
@@ -0,0 +1,17 @@
+package ScannerClass_BufferDemo;
+import java.util.Scanner;
+public class ScannerClassDemo {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter your name");
+		String name=scan.nextLine();
+		System.out.println(name);
+		System.out.println("Enter your age");
+		int age=scan.nextInt();
+		System.out.println(age);
+		scan.close();
+
+	}
+
+}
+
