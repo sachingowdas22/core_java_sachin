7061f8abc91904d0aed3fe529ca4d76129a7f239
harshith <gharshith578@gmail.com>
2025-09-18 15:12:58 +0530

    5th commit

---
+++ src/sr_developers/Kia_Internal.java
@@ -0,0 +1,9 @@
+package sr_developers;
+import org.tnsif.sf.c2tc.junior_developers.*;
+public class Kia_Internal {
+public static void main(String[] args) {
+		
+		Kia_speedlimit obj1=new Kia_speedlimit();
+		obj1.disp_speedlimit();
+	}
+}
