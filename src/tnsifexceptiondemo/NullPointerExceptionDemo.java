0b51d8b61f75885fb4b6e3dcc527da5880678d76
harshith <gharshith578@gmail.com>
2025-09-18 19:07:07 +0530

    6th commit

---
+++ src/tnsifexceptiondemo/NullPointerExceptionDemo.java
@@ -0,0 +1,18 @@
+package tnsifexceptiondemo;
+
+public class NullPointerExceptionDemo {
+	public static void main(String[] args) {
+		String words=null;
+		try {
+			System.out.println(words.length());
+		}catch(NullPointerException e)
+		{
+			System.out.println(e);
+		}
+		finally
+		{
+			System.out.println("No matterwhat this block will get executed");
+		}
+		
+	}
+}
---
+++ src/tnsifexceptiondemo/StringIndexOutBoundException.java
@@ -0,0 +1,23 @@
+package tnsifexceptiondemo;
+
+public class StringIndexOutBoundException {
+	public static void main(String[] args) {
+		String str="hello";
+		try
+		{
+			char ch=str.charAt(5);
+			System.out.println(ch);
+		}catch(StringIndexOutOfBoundsException e)
+		{
+			System.out.println(e);
+			
+		}
+		finally
+		{
+			System.out.println("Thank you");
+		}
+		
+		
+
+	}
+}
